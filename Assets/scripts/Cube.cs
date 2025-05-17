000fcc39f6732bfefd8aae654292e03af0796061
nanana <118258288+nanana1714@users.noreply.github.com>
2025-05-17 15:31:42 +0900

    cubeが動けるようになったよ

---
+++ Assets/scripts/Cube.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Cube : MonoBehaviour
+{
+    float now_speed = 10f;
+    public float jumpForce = 5f;       // ÉWÉÉÉìÉvóÕ
+    private Rigidbody rb;
+    private bool isGrounded = true;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        { 
+            this.transform.position += this.transform.forward * now_speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            this.transform.position += this.transform.right * -now_speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            this.transform.position += this.transform.forward * -now_speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            this.transform.position += this.transform.right * now_speed * Time.deltaTime;
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+
+    }
+    // ínñ Ç∆ÇÃê⁄êGîªíË
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Plane")
+        {
+            isGrounded = true;
+        }
+    }
+}
